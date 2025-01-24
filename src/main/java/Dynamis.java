086c60b8256ae8a40296fcd8f31fdbfaf71a4e54
Zerolegacy <>
2025-01-24 11:30:17 +0800

    Renamed the bot and edited welcome message accordingly.

---
+++ src/main/java/Dynamis.java
@@ -0,0 +1,6 @@
+public class Dynamis {
+    public static void main(String[] args) {
+        String horLine = "----------------------------------------\n";
+        System.out.println(horLine + "Hello! I'm Dynamis\nWhat can I do for you?\n" + horLine);
+    }
+}
