cf3cdb36bf19ac2b69693296f57e0df463ce0432
Zerolegacy <>
2025-01-24 11:45:28 +0800

    Added echo feature

--- src/main/java/Dynamis.java
+++ src/main/java/Dynamis.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Dynamis {
     public static void main(String[] args) {
         String horLine = "----------------------------------------\n";
         System.out.println(horLine + "Hello! I'm Dynamis\nWhat can I do for you?\n" + horLine);
+
+        Scanner scanner = new Scanner(System.in);
+        String echoInput = scanner.nextLine();
+        while (!echoInput.equals("bye")) {
+            System.out.println(horLine + echoInput + "\n" + horLine);
+            echoInput = scanner.nextLine();
+        }
+        System.out.println(horLine + "Bye. Hope to see you again soon!\n" + horLine);
     }
 }
