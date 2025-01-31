17908a0693499e51162df407dac70ee0462cf4a7
Zerolegacy <>
2025-01-31 12:15:43 +0800

    Add DateTime parsing

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,33 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
-    protected String start;
-    protected String end;
+    protected String startString;
+    protected String endString;
+    protected LocalDate start;
+    protected LocalDate end;
 
     public Event(String name, String start, String end) {
         super(name);
-        this.start = start;
-        this.end = end;
+        try {
+            this.start = LocalDate.parse(start);
+        } catch (DateTimeParseException e) {
+            this.start = null;
+            this.startString = start;
+        }
+        try {
+            this.end = LocalDate.parse(end);
+        } catch (DateTimeParseException e) {
+            this.end = null;
+            this.endString = end;
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
+        return "[E]" + super.toString() + " (from: "
+                + (start != null ? start.format(DateTimeFormatter.ofPattern("MMM d yyyy")) : startString) + " to: "
+                + (end != null ? end.format(DateTimeFormatter.ofPattern("MMM d yyyy")) : endString) + ")";
     }
 }
